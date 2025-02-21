9ef79d521c42c0fa0220064be261b8d17965c237
Sora339 <xzhouzhi@gmail.com>
2025-02-22 05:02:24 +0900

    feat: bootstrap

---
+++ lib/hello-cdk-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class HelloCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new Table(this, "items", {
+      partitionKey: {
+        name: "itemId",
+        type: AttributeType.STRING,
+      },
+      tableName: "items",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+  }
+}
